603ab5260056e1918b141c8a8370874e6cdff61c
alyssadev <alyssa.dev.smith@gmail.com>
2022-01-11 09:41:35 +1000

    now with packages

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="voltage_divider",
+    version="0.1.0",
+    description="A Python library to simplify the calculations necessary for adding a voltage divider to your circuit.",
+    url="https://github.com/alyssadev/voltagedivider",
+    author="Alyssa Smith",
+    author_email="alyssa.dev.smith@gmail.com",
+    license="MIT",
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: MIT License'
+    ]
+)
